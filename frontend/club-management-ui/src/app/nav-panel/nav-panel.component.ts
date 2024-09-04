27ab283939b517b2ab7b21c855db41ae431d8a9f
Dreaserous <dreaserous2.0@gmail.com>
2024-09-04 11:50:09 +0530

    Removing Standalone status

--- frontend/club-management-ui/src/app/nav-panel/nav-panel.component.ts
+++ frontend/club-management-ui/src/app/nav-panel/nav-panel.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav-panel',
-  standalone: true,
-  imports: [],
   templateUrl: './nav-panel.component.html',
   styleUrl: './nav-panel.component.css'
 })
