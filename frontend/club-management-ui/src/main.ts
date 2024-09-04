62b2f6a037e47282b39f0e5b836190f1dd468495
Dreaserous <dreaserous2.0@gmail.com>
2024-09-04 10:32:50 +0530

    adding appcomponent to main.ts

--- frontend/club-management-ui/src/main.ts
+++ frontend/club-management-ui/src/main.ts
@@ -3,5 +3,5 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 // import { AppModule } from './app/app.module';
 
-bootstrapApplication(AppModule, appConfig)
+bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
