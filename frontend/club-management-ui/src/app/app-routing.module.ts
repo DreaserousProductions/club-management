9f87683fa2e01934357de17b66460459297b920b
Dreaserous <dreaserous2.0@gmail.com>
2024-09-04 10:44:59 +0530

    Testing routing.module.ts

--- frontend/club-management-ui/src/app/app-routing.module.ts
+++ frontend/club-management-ui/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppModule } from './app.module';
 // import { HomeComponent } from './home/home.component';
 // import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
-    // { path: '', component: HomeComponent },
+    { path: '', component: AppModule },
     // { path: 'about', component: AboutComponent },
     // other routes 
 ];
