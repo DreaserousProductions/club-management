1b61de90ff9e37a57b47a234d03b9b6ff14fc3f7
Dreaserous <dreaserous2.0@gmail.com>
2024-09-04 10:48:50 +0530

    Standalone component

--- frontend/club-management-ui/src/app/app.component.ts
+++ frontend/club-management-ui/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // standalone: true,
-  // imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
