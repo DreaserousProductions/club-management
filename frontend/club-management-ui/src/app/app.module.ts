4f0cecf4b5710aca8713134a76baa81088659c8a
Dreaserous <dreaserous2.0@gmail.com>
2024-09-04 11:49:25 +0530

    Adding component to ngModule

--- frontend/club-management-ui/src/app/app.module.ts
+++ frontend/club-management-ui/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module'; // Import routing module
+import { NavPanelComponent } from './nav-panel/nav-panel.component';
 
 @NgModule({
     declarations: [
         AppComponent, // Declare the component here
+        NavPanelComponent
         // Other components
     ],
     imports: [
