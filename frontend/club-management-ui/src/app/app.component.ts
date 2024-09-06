85be0da8e6da4db2d90380c00c9483bee56f32eb
Dreaserous <dreaserous2.0@gmail.com>
2024-09-06 22:21:25 +0530

    Circular menu trouble shooting #take3

--- frontend/club-management-ui/src/app/app.component.ts
+++ frontend/club-management-ui/src/app/app.component.ts
@@ -9,6 +9,23 @@ import { NavPanelComponent } from './nav-panel/nav-panel.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'club-management-ui';
+export class AppComponent implements AfterViewInit {
+
+  ngAfterViewInit() {
+    this.loadExternalScript('assets/js/CircularMenu.js').then(() => {
+      console.log('Script loaded successfully.');
+    }).catch(() => {
+      console.log('Script loading failed.');
+    });
+  }
+
+  loadExternalScript(scriptUrl: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const scriptElement = document.createElement('script');
+      scriptElement.src = scriptUrl;
+      scriptElement.onload = resolve;
+      scriptElement.onerror = reject;
+      document.body.appendChild(scriptElement);
+    });
+  }
 }
