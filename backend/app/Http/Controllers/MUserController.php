9ba2738e0e9458dbb9dd285a8ced2ba7882d98f2
Dreaserous <dreaserous2.0@gmail.com>
2024-09-08 22:30:05 +0530

    Test controller code for users

--- backend/app/Http/Controllers/MUserController.php
+++ backend/app/Http/Controllers/MUserController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\MUser;
 
 class MUserController extends Controller
 {
-    //
+    public function index()
+    {
+        $users = MUser::all();
+        return response()->json($users);
+    }
+    public function store(Request $request)
+    {
+        $user = MUser::create($request->all());
+        return response()->json($user, 201);
+    }
 }
