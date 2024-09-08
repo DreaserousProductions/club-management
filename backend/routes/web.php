d6c22da8f4c9e51796d7ffb8725033a6a6192f65
Dreaserous <dreaserous2.0@gmail.com>
2024-09-08 22:32:25 +0530

    defining api routes for musers

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MUserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users', [MUserController::class, 'index']);
+Route::post('/users', [MUserController::class, 'store']);
