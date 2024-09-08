954f86572f9fada10cca8f823a755c3c68569c7e
DreaserousProductions <ec2-user@ip-172-31-14-36.ec2.internal>
2024-09-08 16:34:04 +0000

    Making first migration for user table

--- backend/database/migrations/2024_09_08_163331_create_m_users_table.php
+++ backend/database/migrations/2024_09_08_163331_create_m_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user_credentials', function (Blueprint $table) {
+        Schema::create('m_users', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user_credentials');
+        Schema::dropIfExists('m_users');
     }
 };
