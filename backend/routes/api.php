6236ee692e7cb10b5f602134e3732be0936aa3fb
Dreaserous <dreaserous2.0@gmail.com>
2024-09-09 16:27:00 +0530

    Remaking Api.php

--- backend/routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MUserController;
-
-Route::get('/users', [MUserController::class, 'index']);
-Route::post('/users', [MUserController::class, 'store']);
