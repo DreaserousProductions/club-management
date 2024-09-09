0a8726ae8000e52b895b4f258c2670777f0e32dc
Dreaserous <dreaserous2.0@gmail.com>
2024-09-09 11:56:45 +0530

    CORS proxy update test take#6

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\MUserController;
+
+Route::get('/users', [MUserController::class, 'index']);
+Route::post('/users', [MUserController::class, 'store']);
