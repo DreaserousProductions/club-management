d4943158e8641357981b513422733b97bfa52c38
Dreaserous <dreaserous2.0@gmail.com>
2024-09-09 12:05:15 +0530

    CORS proxy update test take#7

--- backend/app/Http/Controllers/MUserController.php
+++ backend/app/Http/Controllers/MUserController.php
@@ -3,20 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
 use App\Models\MUser;
 
 class MUserController extends Controller
 {
     public function index()
     {
+        Log::info('Request Data:', ['request' => $request->all()]);
         $users = MUser::all();
         return response()->json($users);
     }
     public function store(Request $request)
     {
-        print("I got here");
+        Log::info('Request Data:', ['request' => $request->all()]);
         $user = MUser::create($request->all());
-        print("I got here");
         return response()->json($user, 201);
     }
 }
