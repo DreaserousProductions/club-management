c459748f4d0f0c0627f9b6887522d1b4b3ca62c1
Dreaserous <dreaserous2.0@gmail.com>
2024-09-09 12:10:30 +0530

    CORS proxy update test take#9

--- backend/app/Http/Controllers/MUserController.php
+++ backend/app/Http/Controllers/MUserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 use App\Models\MUser;
 
@@ -18,6 +19,25 @@ class MUserController extends Controller
     public function store(Request $request)
     {
         Log::info('Request Data:', ['request' => $request->all()]);
+
+        // Validate request data
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'username' => 'required|string|max:255|unique:users,username',
+            'password' => 'required|string|min:8',
+            'role' => 'required|integer',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // Hash the password
+        $request->merge([
+            'password' => Hash::make($request->input('password')),
+        ]);
+
         $user = MUser::create($request->all());
         return response()->json($user, 201);
     }
