1ff39600d61b670956c9d129c4f122dbc6c91ef2
Dreaserous <dreaserous2.0@gmail.com>
2024-09-10 01:51:51 +0530

    mingle users tble migration

--- backend/database/migrations/2024_09_09_193002_create_mingle_users_table.php
+++ backend/database/migrations/2024_09_09_193002_create_mingle_users_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('mingle_users', function (Blueprint $table) {
             $table->id();
+            $table->string('rollnumber')->unique(); // Mandatory roll number
+            $table->string('role')->nullable(); // Nullable role
+            $table->string('otp')->nullable(); // Nullable otp
+            $table->string('name')->nullable(); // Nullable name
+            $table->string('avatar_img_path')->nullable(); // Nullable avatar image path
+            $table->json('clubs_enrolled_in')->nullable(); // Nullable JSON list of club IDs
+            $table->unsignedBigInteger('club_admin')->nullable(); // Nullable club admin (club ID)
             $table->timestamps();
         });
     }
