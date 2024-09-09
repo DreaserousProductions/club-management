66d596a8dda753e07148c8628321deaa5a5b6349
Dreaserous <dreaserous2.0@gmail.com>
2024-09-09 16:29:27 +0530

    remaking api.php part 3

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MUserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/users', [MUserController::class, 'index']);
+Route::post('/users', [MUserController::class, 'store']);
