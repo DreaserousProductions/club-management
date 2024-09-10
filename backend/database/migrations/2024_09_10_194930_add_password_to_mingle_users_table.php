979ab5191de4702397a2e40c36c989ee11c5f1c5
Dreaserous <dreaserous2.0@gmail.com>
2024-09-11 01:21:10 +0530

    Registration Complete #take3

--- backend/database/migrations/2024_09_10_194930_add_password_to_mingle_users_table.php
+++ backend/database/migrations/2024_09_10_194930_add_password_to_mingle_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('mingle_users', function (Blueprint $table) {
-            //
+            $table->string('password')->nullable()->after('registered');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('mingle_users', function (Blueprint $table) {
-            //
+            $table->dropColumn('password');
         });
     }
 };
