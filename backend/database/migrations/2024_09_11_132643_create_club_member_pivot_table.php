048a60e8525014471edb2691a6e207139107a418
Dreaserous <dreaserous2.0@gmail.com>
2024-09-11 18:58:42 +0530

    Making club_member_pivot migration

--- backend/database/migrations/2024_09_11_132643_create_club_member_pivot_table.php
+++ backend/database/migrations/2024_09_11_132643_create_club_member_pivot_table.php
@@ -8,20 +8,30 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
-        Schema::create('club_member_pivot', function (Blueprint $table) {
+        Schema::create('club_member', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('club_id');
+            $table->unsignedBigInteger('member_id');
             $table->timestamps();
+
+            // Foreign key constraints
+            $table->foreign('club_id')->references('id')->on('clubs')->onDelete('cascade');
+            $table->foreign('member_id')->references('id')->on('mingle_users')->onDelete('cascade');
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('club_member_pivot');
+        Schema::dropIfExists('club_member');
     }
 };
