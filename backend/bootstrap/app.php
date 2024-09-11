65881323309a5b93957f78c0d919b0b4604b69c2
Dreaserous <dreaserous2.0@gmail.com>
2024-09-11 20:04:29 +0530

    JWT TESTING

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -11,9 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
-    })
+    ->withMiddleware([
+        JWTAuthenticate::class, // Ensure JWT Middleware is applied
+    ])
+    // ->withMiddleware(function (Middleware $middleware) {
+    //     //
+    // })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
