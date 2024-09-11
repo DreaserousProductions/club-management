155f4ff6cf087e6c3a926fa6a2a3576958464df6
Dreaserous <dreaserous2.0@gmail.com>
2024-09-11 19:39:25 +0530

    TESTING JWT MIDDLEWARE

--- backend/config/auth.php
+++ backend/config/auth.php
@@ -40,6 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'api' => [
+            'driver' => 'jwt',
+            'provider' => 'clubs',
+        ],
     ],
 
     /*
@@ -65,10 +70,10 @@ return [
             'model' => env('AUTH_MODEL', App\Models\User::class),
         ],
 
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
+        'clubs' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Club::class,
+        ],
     ],
 
     /*
