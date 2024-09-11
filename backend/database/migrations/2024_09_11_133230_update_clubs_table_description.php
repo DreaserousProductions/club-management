3bbbbdd011f2e08155c6c824f0cf10a20af9ff41
Dreaserous <dreaserous2.0@gmail.com>
2024-09-11 19:03:39 +0530

    update clubs table migration

--- backend/database/migrations/2024_09_11_133230_update_clubs_table_description.php
+++ backend/database/migrations/2024_09_11_133230_update_clubs_table_description.php
@@ -8,17 +8,33 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
-        //
+        Schema::table('clubs', function (Blueprint $table) {
+            // Drop the old 'description' column
+            $table->dropColumn('description');
+
+            // Add the new 'description' column with JSON datatype
+            $table->json('description')->nullable();
+        });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
-        //
+        Schema::table('clubs', function (Blueprint $table) {
+            // Rollback the new 'description' column
+            $table->dropColumn('description');
+
+            // Add the old 'description' column back as a text type
+            $table->text('description')->nullable();
+        });
     }
 };
